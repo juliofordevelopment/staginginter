015620135494b5464ac6ce06be0d3e16dde3bbf1
Julio Colomer <julio.colomer@accellion.com>
2013-03-05 12:33:52 -0500

    updated template.php to include changes in old search functionality

--- sites/all/themes/accellion/template.php
+++ sites/all/themes/accellion/template.php
@@ -143,3 +143,31 @@ function mymodule_preproces_page(&$vars) {
     $vars['page_title'] = render($view['field_page_title']);
   }
 }
+
+function accellion_preprocess_search_result(&$variables) {
+  global $language;
+
+  $result = $variables['result'];
+  $variables['url'] = check_url($result['link']);
+  $variables['title'] = check_plain($result['title']);
+  if (isset($result['language']) && $result['language'] != $language->language && $result['language'] != LANGUAGE_NONE) {
+    $variables['title_attributes_array']['xml:lang'] = $result['language'];
+    $variables['content_attributes_array']['xml:lang'] = $result['language'];
+  }
+
+  $info = array();
+  if (!empty($result['module'])) {
+    $info['module'] = check_plain($result['module']);
+  }
+
+  if (isset($result['extra']) && is_array($result['extra'])) {
+    $info = array_merge($info, $result['extra']);
+  }
+  // Check for existence. User search does not include snippets.
+  $variables['snippet'] = isset($result['snippet']) ? $result['snippet'] : '';
+  // Provide separated and grouped meta information..
+  $variables['info_split'] = $info;
+  $variables['info'] = implode(' - ', $info);
+  $variables['theme_hook_suggestions'][] = 'search_result__' . $variables['module'];
+}
+
