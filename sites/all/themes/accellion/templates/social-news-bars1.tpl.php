1a5e6a909bc599a99a1f117ce8c9e2dbd4dda79a
Julio Colomer <julio.colomer@accellion.com>
2013-03-14 13:02:02 -0400

    changed bottom ad in tpl to show Gartner

--- sites/all/themes/accellion/templates/social-news-bars1.tpl.php
+++ sites/all/themes/accellion/templates/social-news-bars1.tpl.php
@@ -30,8 +30,8 @@ $view = views_get_view('tab_views_whitepaper');
 print $view->preview('default');
 ?></span>
     </div>
-    <div class="grid-child grid-10"><a href="/company/events">
-<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-lg.png" border="0"></a>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/wp-gartner-marketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-Gartner.png" border="0"></a>
     </div>
 </div>
 
