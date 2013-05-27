45782c2057e80af81a9fa871876bcddf328eeb92
Julio Colomer iMac <juliofordevelopment@gmail.com>
2013-05-27 11:26:54 -0700

    changes for French version

--- sites/all/themes/accellion/templates/social-news-bars1.tpl.php
+++ sites/all/themes/accellion/templates/social-news-bars1.tpl.php
@@ -120,28 +120,28 @@ print $view->preview('default');
 
 <div class="grid-parent">
     <div class="grid-child grid-30">
-        <span class="grid-30-t"><b>FR-Recent News</b></span>
+        <span class="grid-30-t"><b>Dernières actualités</b></span>
         <br><span style="word-wrap: break-word;"><?php
 $view = views_get_view('press_release_view');
 print $view->preview('social_news_pr_block');
 ?></span>
     </div>
     <div class="grid-child grid-30">
-        <span class="grid-30-t"><b>Customer Case Studies</b></span>
+        <span class="grid-30-t"><b>Études de cas de clients</b></span>
         <br><span style="word-wrap: break-word;"><?php
 $view = views_get_view('tab_views_ccs');
 print $view->preview('block');
 ?></span>
     </div>
     <div class="grid-child grid-30">
-        <span class="grid-30-t"><b>Industry Whitepapers</b></span>
+        <span class="grid-30-t"><b>Livres blancs du secteur</b></span>
         <br><span style="word-wrap: break-word;"><?php
 $view = views_get_view('tab_views_whitepaper');
 print $view->preview('default');
 ?></span>
     </div>
     <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
-<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-Gartner.png" border="0"></a>
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-lg-FRv2-05242013.jpg" border="0"></a>
     </div>
 </div>
 
