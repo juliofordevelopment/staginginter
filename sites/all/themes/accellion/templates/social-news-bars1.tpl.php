144827039df3bd0648a39ca4f2f38a6c8d633e9c
Julio Colomer iMac <juliofordevelopment@gmail.com>
2013-05-27 20:04:39 -0700

    changes for bottom boxes and ad

--- sites/all/themes/accellion/templates/social-news-bars1.tpl.php
+++ sites/all/themes/accellion/templates/social-news-bars1.tpl.php
@@ -78,6 +78,74 @@ print $view->preview('default');
 <?php endif; ?>
 
 
+<!--Australia -->
+
+<?php if ($language->language == 'en-AU'): ?>
+
+<div class="grid-parent">
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Recent News</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('press_release_view');
+print $view->preview('social_news_pr_block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Customer Case Studies</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_ccs');
+print $view->preview('block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Industry Whitepapers</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_whitepaper');
+print $view->preview('default');
+?></span>
+    </div>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-Gartner.png" border="0"></a>
+    </div>
+</div>
+
+
+<?php endif; ?>
+
+
+<!--UK -->
+
+<?php if ($language->language == 'en-GB'): ?>
+
+<div class="grid-parent">
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Recent News</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('press_release_view');
+print $view->preview('social_news_pr_block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Customer Case Studies</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_ccs');
+print $view->preview('block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Industry White papers</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_whitepaper');
+print $view->preview('default');
+?></span>
+    </div>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-lg-UK-05242013.jpg" border="0"></a>
+    </div>
+</div>
+
+
+<?php endif; ?>
 
 <!--Canada English -->
 
@@ -113,8 +181,113 @@ print $view->preview('default');
 
 <?php endif; ?>
 
+<!--HK English -->
+
+<?php if ($language->language == 'en-HK'): ?>
+
+<div class="grid-parent">
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Recent News</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('press_release_view');
+print $view->preview('social_news_pr_block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Customer Case Studies</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_ccs');
+print $view->preview('block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Industry Whitepapers</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_whitepaper');
+print $view->preview('default');
+?></span>
+    </div>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-Gartner.png" border="0"></a>
+    </div>
+</div>
 
 
+<?php endif; ?>
+
+
+<!--IN English -->
+
+<?php if ($language->language == 'en-IN'): ?>
+
+<div class="grid-parent">
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Recent News</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('press_release_view');
+print $view->preview('social_news_pr_block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Customer Case Studies</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_ccs');
+print $view->preview('block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Industry Whitepapers</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_whitepaper');
+print $view->preview('default');
+?></span>
+    </div>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-Gartner.png" border="0"></a>
+    </div>
+</div>
+
+
+<?php endif; ?>
+
+
+<!--SG English -->
+
+<?php if ($language->language == 'en-SG'): ?>
+
+<div class="grid-parent">
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Recent News</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('press_release_view');
+print $view->preview('social_news_pr_block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Customer Case Studies</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_ccs');
+print $view->preview('block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Industry Whitepapers</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_whitepaper');
+print $view->preview('default');
+?></span>
+    </div>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-Gartner.png" border="0"></a>
+    </div>
+</div>
+
+
+<?php endif; ?>
+
+
+
+<!-- France -->
 
 <?php if ($language->language == 'fr'): ?>
 
@@ -142,6 +315,147 @@ print $view->preview('default');
     </div>
     <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
 <img src="/sites/default/files/events/Accellion-Social-Bar-Banner-lg-FRv2-05242013.jpg" border="0"></a>
+    </div>
+</div>
+
+
+<?php endif; ?>
+
+
+<!-- CA French -->
+
+<?php if ($language->language == 'fr-CA'): ?>
+
+<div class="grid-parent">
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Dernières actualités</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('press_release_view');
+print $view->preview('social_news_pr_block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Études de cas de clients</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_ccs');
+print $view->preview('block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Livres blancs du secteur</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_whitepaper');
+print $view->preview('default');
+?></span>
+    </div>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-lg-FRv2-05242013.jpg" border="0"></a>
+    </div>
+</div>
+
+
+<?php endif; ?>
+
+
+<!-- CH French -->
+
+<?php if ($language->language == 'fr-CH'): ?>
+
+<div class="grid-parent">
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Dernières actualités</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('press_release_view');
+print $view->preview('social_news_pr_block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Études de cas de clients</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_ccs');
+print $view->preview('block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Livres blancs du secteur</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_whitepaper');
+print $view->preview('default');
+?></span>
+    </div>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-lg-FRv2-05242013.jpg" border="0"></a>
+    </div>
+</div>
+
+
+<?php endif; ?>
+
+
+
+<!-- Germany -->
+
+<?php if ($language->language == 'de'): ?>
+
+<div class="grid-parent">
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Aktuelle Nachrichten</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('press_release_view');
+print $view->preview('social_news_pr_block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Kundenfallstudien</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_ccs');
+print $view->preview('block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Weißbücher der Branche</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_whitepaper');
+print $view->preview('default');
+?></span>
+    </div>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-lg-DE-05242013.jpg" border="0"></a>
+    </div>
+</div>
+
+
+<?php endif; ?>
+
+
+<!-- de CH -->
+
+<?php if ($language->language == 'de-CH'): ?>
+
+<div class="grid-parent">
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Aktuelle Nachrichten</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('press_release_view');
+print $view->preview('social_news_pr_block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Kundenfallstudien</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_ccs');
+print $view->preview('block');
+?></span>
+    </div>
+    <div class="grid-child grid-30">
+        <span class="grid-30-t"><b>Weißbücher der Branche</b></span>
+        <br><span style="word-wrap: break-word;"><?php
+$view = views_get_view('tab_views_whitepaper');
+print $view->preview('default');
+?></span>
+    </div>
+    <div class="grid-child grid-10"><a href="http://www.info.accellion.com/WP-Hi-GartnerMarketscope.html?ms=bottomad0213">
+<img src="/sites/default/files/events/Accellion-Social-Bar-Banner-lg-DE-05242013.jpg" border="0"></a>
     </div>
 </div>
 
